c3f030b6639dd065512580573898aecac6a80c9a
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2021-06-08 09:55:09 -0300

    Calendario ok

--- mypet_app/lib/lista.dart
+++ mypet_app/lib/lista.dart
@@ -21,25 +21,28 @@ class _Lista extends State<Lista> {
   TextEditingController _controllerPesquisa = TextEditingController();
   List<DocumentSnapshot> r = null;
   bool _loading = true;
-  Map<DateTime, List<Event>> selectedEvents;
+  Map<DateTime, List<Event>> selectedEventsU;
+  Map<DateTime, List<Event>> selectedEventsP;
   CalendarFormat format = CalendarFormat.month;
   DateTime selectedDay = DateTime.now();
   DateTime focusedDay = DateTime.now();
   static final LatLng _kMapCenter =
-  LatLng(19.018255973653343, 72.84793849278007);
+      LatLng(19.018255973653343, 72.84793849278007);
   TextEditingController _controllerNome = TextEditingController();
   TextEditingController _controllerUltima = TextEditingController();
   TextEditingController _controllerProxima = TextEditingController();
 
   static final CameraPosition _kInitialPosition =
-  CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
+      CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
   GoogleMapController _controller;
 
   @override
   void initState() {
     print(widget.idUser + widget.idPet);
-    selectedEvents = {};
-    selectedEvents[selectedDay] = [];
+    selectedEventsU = {};
+    selectedEventsU[selectedDay] = [];
+    selectedEventsP = {};
+    selectedEventsP[selectedDay] = [];
     super.initState();
   }
 
@@ -78,9 +81,9 @@ class _Lista extends State<Lista> {
             ),
             title: Text("MyPet  \u{1F43E}"),
           ),
-          body: TabBarView(
-              children: [body(snap), map(snap), calendario(snap)]),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          body: TabBarView(children: [body(snap), map(snap), calendario(snap)]),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.startFloat,
           floatingActionButton: FloatingActionButton(
               backgroundColor: Colors.lightGreen,
               elevation: 6,
@@ -103,7 +106,9 @@ class _Lista extends State<Lista> {
                               onPressed: () async {
                                 bool formOk = _formKey.currentState.validate();
                                 if (formOk) {
-                                  await FirebaseFirestore.instance.collection('eventos').add({
+                                  await FirebaseFirestore.instance
+                                      .collection('eventos')
+                                      .add({
                                     'nome': _controllerNome.text,
                                     'ultima': _controllerUltima.text,
                                     'proxima': _controllerProxima.text,
@@ -148,7 +153,8 @@ class _Lista extends State<Lista> {
               color: Colors.black,
             ),
             decoration: InputDecoration(
-                labelText: "Última dose:", hintText: "Digite o valor da última dose"),
+                labelText: "Última dose:",
+                hintText: "Digite o valor da última dose"),
             controller: _controllerUltima,
             keyboardType: TextInputType.datetime,
             validator: (String text) {
@@ -165,12 +171,11 @@ class _Lista extends State<Lista> {
               color: Colors.black,
             ),
             decoration: InputDecoration(
-                labelText: "Próxima dose:", hintText: "Digite o valor da próxima dose"),
+                labelText: "Próxima dose:",
+                hintText: "Digite o valor da próxima dose"),
             controller: _controllerProxima,
             keyboardType: TextInputType.datetime,
             validator: (String text) {
-
-
               return null;
             },
           ),
@@ -192,7 +197,8 @@ class _Lista extends State<Lista> {
               color: Colors.black,
             ),
             decoration: InputDecoration(
-                labelText: "Última dose:", hintText: "Digite o valor da última dose"),
+                labelText: "Última dose:",
+                hintText: "Digite o valor da última dose"),
             controller: _controllerUltima,
             keyboardType: TextInputType.datetime,
             validator: (String text) {
@@ -209,12 +215,11 @@ class _Lista extends State<Lista> {
               color: Colors.black,
             ),
             decoration: InputDecoration(
-                labelText: "Próxima dose:", hintText: "Digite o valor da próxima dose"),
+                labelText: "Próxima dose:",
+                hintText: "Digite o valor da próxima dose"),
             controller: _controllerProxima,
             keyboardType: TextInputType.datetime,
             validator: (String text) {
-
-
               return null;
             },
           ),
@@ -224,8 +229,8 @@ class _Lista extends State<Lista> {
   }
 
   calendario(var snap) {
-    if (selectedEvents[selectedDay] == null ||
-        selectedEvents[selectedDay].length == 0)
+    if (selectedEventsU[selectedDay] == null ||
+        selectedEventsU[selectedDay].length == 0)
       return Column(
         children: [
           TableCalendar(
@@ -255,7 +260,7 @@ class _Lista extends State<Lista> {
               return isSameDay(selectedDay, date);
             },
 
-            eventLoader: _getEventsfromDay,
+            eventLoader: _getEventsfromDayU,
 
             //To style the Calendar
             calendarStyle: CalendarStyle(
@@ -296,7 +301,7 @@ class _Lista extends State<Lista> {
         ],
       );
 
-    print(selectedEvents[selectedDay].length);
+    print(selectedEventsU[selectedDay].length);
     return Column(
       children: [
         TableCalendar(
@@ -326,7 +331,7 @@ class _Lista extends State<Lista> {
             return isSameDay(selectedDay, date);
           },
 
-          eventLoader: _getEventsfromDay,
+          eventLoader: _getEventsfromDayU,
 
           //To style the Calendar
           calendarStyle: CalendarStyle(
@@ -365,28 +370,55 @@ class _Lista extends State<Lista> {
           ),
         ),
         Container(
-            child: Text("\nAniversariantes:",
+            child: Text("\nÚltima Dose:",
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 25,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ))),
         Flexible(
             child: ListView.builder(
                 padding: const EdgeInsets.all(8),
-                itemCount: selectedEvents[selectedDay].length,
+                itemCount: selectedEventsU[selectedDay].length,
                 itemBuilder: (BuildContext context, int index) {
                   return Expanded(
                       child: Container(
                     //margin: ,
-                    height: 50,
+                    height: 30,
                     child: Expanded(
                         child: Center(
                             child: Text(
-                      selectedEvents[selectedDay][index].title + "\n",
+                      selectedEventsU[selectedDay][index].title + "\n",
                       style: TextStyle(
                         color: Colors.blueGrey,
-                        fontSize: 25,
+                        fontSize: 20,
+                      ),
+                    ))),
+                  ));
+                })),
+        Container(
+            child: Text("\nPróxima Dose:",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ))),
+        Flexible(
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: selectedEventsP[selectedDay].length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Expanded(
+                      child: Container(
+                    //margin: ,
+                    height: 30,
+                    child: Expanded(
+                        child: Center(
+                            child: Text(
+                      selectedEventsP[selectedDay][index].title + "\n",
+                      style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 20,
                       ),
                     ))),
                   ));
@@ -400,19 +432,19 @@ class _Lista extends State<Lista> {
       initialCameraPosition: _kInitialPosition,
       onMapCreated: onMapCreated,
       myLocationEnabled: true,
-     // useCurrentLocation: true,
-     // selectInitialPosition: true,
+      myLocationButtonEnabled: true,
+
+
+      // selectInitialPosition: true,
     );
     /*return */
   }
-
 
   void onMapCreated(GoogleMapController controller) async {
     _controller = controller;
     String value = await DefaultAssetBundle.of(context)
         .loadString('assets/map_style.json');
     _controller.setMapStyle(value);
-    myLocationEnabled: true;
   }
 
   body(var snap) {
@@ -446,16 +478,14 @@ class _Lista extends State<Lista> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Container(
-
-                                  child: Text(
-                                    item['nome'],
-                                    style: TextStyle(
-                                      color: Colors.black87,
-                                      fontSize: 18,
-
-                                    ),
+                                child: Text(
+                                  item['nome'],
+                                  style: TextStyle(
+                                    color: Colors.black87,
+                                    fontSize: 18,
                                   ),
                                 ),
+                              ),
                               Row(
                                 children: [
                                   Text(
@@ -463,7 +493,6 @@ class _Lista extends State<Lista> {
                                     style: TextStyle(
                                       color: Colors.black87,
                                       fontSize: 13,
-
                                     ),
                                   ),
                                   Text(
@@ -471,7 +500,6 @@ class _Lista extends State<Lista> {
                                     style: TextStyle(
                                       color: Colors.blueGrey,
                                       fontSize: 13,
-
                                     ),
                                   ),
                                 ],
@@ -483,7 +511,6 @@ class _Lista extends State<Lista> {
                                     style: TextStyle(
                                       color: Colors.black87,
                                       fontSize: 13,
-
                                     ),
                                   ),
                                   Text(
@@ -491,13 +518,10 @@ class _Lista extends State<Lista> {
                                     style: TextStyle(
                                       color: Colors.blueGrey,
                                       fontSize: 13,
-
                                     ),
                                   ),
                                 ],
                               )
-
-
                             ]),
                         Spacer(),
                         Row(
@@ -545,7 +569,7 @@ class _Lista extends State<Lista> {
     );
   }
 
-  edit(var item){
+  edit(var item) {
     _controllerNome.text = item['nome'];
     _controllerUltima.text = item['ultima'];
     _controllerProxima.text = item['proxima'];
@@ -553,8 +577,7 @@ class _Lista extends State<Lista> {
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text('Editar',
-                style: TextStyle(fontSize: 15)),
+            title: Text('Editar', style: TextStyle(fontSize: 15)),
             content: Container(height: 200, child: form()),
             actions: <Widget>[
               TextButton(
@@ -580,15 +603,14 @@ class _Lista extends State<Lista> {
         });
   }
 
-  update(var item){
+  update(var item) {
     _controllerUltima.text = item['proxima'];
     _controllerProxima.text = '';
     return showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text(item['nome'],
-                style: TextStyle(fontSize: 15)),
+            title: Text(item['nome'], style: TextStyle(fontSize: 15)),
             content: Container(height: 200, child: formDate()),
             actions: <Widget>[
               TextButton(
@@ -614,15 +636,15 @@ class _Lista extends State<Lista> {
   }
 
   void addEvents(snap) async {
+    addEventsP(snap);
     var aux = (await FirebaseFirestore.instance
-            .collection("contato2")
-            .where('excluido', isEqualTo: false)
-            .where('fk', isEqualTo: widget.idUser)
+            .collection("eventos")
+            .where('pk', isEqualTo: widget.idUser + widget.idPet)
             .get())
         .docs;
-    selectedEvents[selectedDay] = [];
+    selectedEventsU[selectedDay] = [];
     for (int i = 0; i < aux.length; i++) {
-      var x = aux[i]['data'].split('/');
+      var x = aux[i]['ultima'].split('/');
       print(x[2] + x[1] + x[0]);
       print(selectedDay.day.toString() +
           " " +
@@ -637,19 +659,58 @@ class _Lista extends State<Lista> {
           selectedDay.month == int.parse(x[1])) {
         print('ENTROU IF');
         setState(() {
-          if (!selectedEvents[selectedDay]
+          if (!selectedEventsU[selectedDay]
               .contains(Event(title: aux[i]['nome']))) {
             print("add dnv");
-            selectedEvents[selectedDay].add(new Event(title: aux[i]['nome']));
-            print(selectedEvents[selectedDay]);
-            print(selectedEvents[selectedDay].length);
+            selectedEventsU[selectedDay].add(new Event(title: aux[i]['nome']));
+            print(selectedEventsU[selectedDay]);
+            print(selectedEventsU[selectedDay].length);
           }
         });
       }
     }
   }
 
-  List<Event> _getEventsfromDay(DateTime date) {
-    return selectedEvents[date] ?? [];
+  List<Event> _getEventsfromDayU(DateTime date) {
+    return selectedEventsU[date] ?? [];
+  }
+
+  void addEventsP(snap) async {
+    var aux = (await FirebaseFirestore.instance
+        .collection("eventos")
+        .where('pk', isEqualTo: widget.idUser + widget.idPet)
+        .get())
+        .docs;
+    selectedEventsP[selectedDay] = [];
+    for (int i = 0; i < aux.length; i++) {
+      var x = aux[i]['proxima'].split('/');
+      print(x[2] + x[1] + x[0]);
+      print(selectedDay.day.toString() +
+          " " +
+          selectedDay.month.toString() +
+          " " +
+          int.parse(x[0]).toString() +
+          " " +
+          int.parse(x[1]).toString());
+      var date = DateTime.parse(x[2] + x[1] + x[0]);
+
+      if (selectedDay.day == int.parse(x[0]) &&
+          selectedDay.month == int.parse(x[1])) {
+        print('ENTROU IF');
+        setState(() {
+          if (!selectedEventsP[selectedDay]
+              .contains(Event(title: aux[i]['nome']))) {
+            print("add dnv");
+            selectedEventsP[selectedDay].add(new Event(title: aux[i]['nome']));
+            print(selectedEventsP[selectedDay]);
+            print(selectedEventsP[selectedDay].length);
+          }
+        });
+      }
+    }
+  }
+
+  List<Event> _getEventsfromDayP(DateTime date) {
+    return selectedEventsP[date] ?? [];
   }
 }
